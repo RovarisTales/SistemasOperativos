c94d5531ae1d4832bd48a43055d480196251b389
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2022-04-23 19:28:12 +0100

    Continuamos no exec

--- sdstore.c
+++ sdstore.c
@@ -14,21 +14,30 @@ int status(){
     return 0;
 }
 
-int procfile(int argc,char *argv[])
-{
+int procfile(int argc,char *argv[]){
+
 
     char* entrada_saida [2];
-    entrada_saida [0]= (char *) malloc(sizeof (argv[0]) + 4);
-    strcpy(entrada_saida[0],"< ");
+    entrada_saida [0]= (char *) malloc(sizeof (argv[0]));
+    //strcpy(entrada_saida[0],"< ");
+    //strcat(entrada_saida[0],argv[0]);
+    /*
+    entrada_saida [0] = (char*) malloc(sizeof ("<"));
+    entrada_saida [0] = "<";
+    entrada_saida [2] = (char*) malloc(sizeof ("<"));
+    entrada_saida [2] = ">";
+    entrada_saida [3]= (char *) malloc(sizeof (argv[1]));
+     */
     entrada_saida [1]= (char *) malloc(sizeof (argv[1]));
     strcat(entrada_saida [0],argv[0]);
     strcpy(entrada_saida [1],argv[1]);
-    strcat(entrada_saida[0]," >");
+    //strcat(entrada_saida[0]," >");
     printf("%s",entrada_saida[0]);
-    printf("%s",entrada_saida[1]);
+    //printf("%s",entrada_saida[1]);
+
+/*
     for (int i = 2 ; i < argc ; i++)
     {
-
         /*
         int pipe_fd[2];
         if(pipe(pipe_fd[0]) < 0)
@@ -43,21 +52,30 @@ int procfile(int argc,char *argv[])
         if (!fork())
         {
             //close(pipe_fd)
+            printf("OIII");
+            fflush(stdout);
             execv(transf ,entrada_saida);
             _exit(1);
         }
     }
-    wait(1);
+*/
+
     return 0;
+
 
 }
 
 int main (int argc, char *argv[]){
 
-    char * str = (char *) malloc(sizeof (argv[1]));
+    /*
+    for (int i = 1 ; i < argc ; i++ )
+    {
+        printf("%s",argv[i]);
+        write(stdout, argv[i] , strlen(argv[i]));
+    }
+    //execv("./SDStore-transf/bcompress",argv+1);
 
-    execv("./SDStore-transf/bcompress ",argv+2);
-
+    */
     switch(strcmp(argv[1],"proc-file"))
     {
         case 0:
@@ -70,6 +88,7 @@ int main (int argc, char *argv[]){
             break;
     }
     //printf("OIiii");
+
 
 
     return 0;
