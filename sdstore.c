1cb74e7c361e5e6ba654f3ef3f383f67f1e8f783
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2022-04-28 17:21:15 +0100

    Distribuição do trabalho

--- sdstore.c
+++ sdstore.c
@@ -10,138 +10,33 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-int status(){
-    printf("KKKKKKKKKKKKKKKKKKKKKKKKKKk VAI SE FUDER KKKKKKK \n");
-    return 0;
-}
-
-int procfile(int argc,char *argv[]){
-    write(STDIN_FILENO,"Procesing\n",10);
-
-    //criar n-2 pipes para os filhos
-    int p[argc-2][2];
-    
-    for (int i = 2 ; i < argc ; i++){
-        //pipe 0 leitura
-        //pipe 1 escrita
-        //cria n-esimo pipe
-        if(i != argc-1){
-            if(pipe(p[i-2]) < 0){
-                perror("pipe");
-                _exit(1);
-            }
-        }
-        char transf [17 + strlen(argv[i])];
-        strcpy(transf,"./SDStore-transf/");
-        strcat(transf ,argv[i]);
-        //primeiro ciclo e 1 argumento
-        if(i == 2 && (i == (argc -1))){
-            
-            if(!fork()){
-                //close(pipe_fd)
-                int fd0 = open(argv[0], O_RDONLY,0666);
-                //ficehiro passa a ser stdin
-                dup2(fd0,0);
-                //close stdout e programa escreve no ficheiro pipe escrita n
-                int fd1 = open(argv[1],O_CREAT|O_WRONLY|O_TRUNC,0666);
-                //stdout passa a ser pipe de escrita n-2
-                dup2(fd1,STDOUT_FILENO);
-                //programa executa
-            
-                execl(transf ,transf,NULL);
-                perror("execl");
-                _exit(1);
-            
-            }
-        }
-        //primeiro ciclo
-        else if(i==2){
-
-            if(!fork()){
-                //close(pipe_fd)
-                //close do pipe que nao vamos usar
-                close(p[i-2][0]);
-                //close stdin e programa le o ficheiro fd0
-                int fd0 = open(argv[0], O_RDONLY,0666);
-                //ficehiro passa a ser stdin
-                dup2(fd0,STDIN_FILENO);
-                //close stdout e programa escreve no ficheiro pipe escrita n
-                //int fd1 = write(argv[1],O_CREAT|O_WRONLY|O_TRUNC,0666);
-                //stdout passa a ser pipe de escrita n-2
-                dup2(p[i-2][1],STDOUT_FILENO);
-                //programa executa
-            
-            
-            execl(transf ,transf,NULL);
-            perror("execl");
-            _exit(1);
-            
-            }
-        }
-        //ciclos seguintes
-        else if ((argc-1)!= i){
-
-            if(!fork()){
-                //close do pipe que nao vamos usar
-                close(p[i-2][0]);
-                //close stdin e programa le o pipe anterior
-                dup2(p[i-3][0],STDIN_FILENO);
-                //ficehiro passa a ser stdin
-            
-                //close stdout e programa escreve no ficheiro pipe escrita n
-                //int fd1 = write(argv[1],O_CREAT|O_WRONLY|O_TRUNC,0666);
-                //stdout passa a ser pipe de escrita n-2
-                dup2(p[i-2][1],STDOUT_FILENO);
-                //programa executa
-                
-                execl(transf ,transf,NULL);
-                perror("execl");
-                _exit(1);
-            
-            }
-        }
-        //ultimo ciclo
-        else{
-
-            if(!fork()){
-
-                int fd1 = open(argv[1],O_CREAT|O_WRONLY|O_TRUNC,0666);
-                dup2(fd1,STDOUT_FILENO);
-                dup2(p[i-3][0],STDIN_FILENO);
-                execl(transf ,transf,NULL);
-                
-                _exit(1);
-            }
-            
-        }
-        //fecha pipe de escrita
-        close(p[i-2][1]);
-        //fecha pipe de leitura anterior
-        if(i!=2)close(p[i-3][0]);
-        if(i == (argc-1))close(p[i-2][0]);
-        wait(NULL);
-        
-    }
- 
-    return 0;
-
-
-}
+//TODO comunicacao entre servidor e cliente -> Tiago
+//TODO status -> Breno
+//TODO atualizar permissoes para fazer proc-file -> Breno
+//TODO atualizar proc-file() -> Tales
+//TODO atualizar argv no servidor para o pipe com nome -> Tales
 
 int main (int argc, char *argv[]){
+    char buffer[1024];
 
-
-    switch(strcmp(argv[1],"proc-file")){
-        case 0:
-            write(STDOUT_FILENO,"Pending\n",8);
-            procfile(argc-2,argv+2);
-            write(STDOUT_FILENO,"Concluded\n",10);
-            break;
-        default:
-            status();
-            break;
+    for (int i = 1; i < argc-1; i++)
+    {
+        strcat(buffer,argv[i]);
     }
+
+    int fd = open("contacto_p_servidor",O_RDWR,0666);
+    write(fd,buffer,strlen(buffer));
+    //TODO while que espera o concluded e cria um FIFO para o processo.
+    while ()
+    {
+        /* code */
+    }
+    
+    close(fd);
+    
 
     return 0;
 
---
+++ sdstored.c
@@ -0,0 +1,156 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+int bcompress_e = 0;
+int bdecompress_e = 0;
+int decrypt_e = 0;
+int encrypt_e = 0;
+int gcompress_e = 0;
+int gdecompress_e = 0;
+int nop_e = 0;
+
+int static id = 0;
+
+
+int main (int argc, char *argv[]){
+
+    mkfifo("contacto",0666);
+
+    int fd = open("contacto",O_RDONLY,0666);
+
+    switch(strcmp(argv[1],"proc-file")){
+        case 0:
+            write(STDOUT_FILENO,"Pending\n",8);
+            procfile(argc-2,argv+2);
+            write(STDOUT_FILENO,"Concluded\n",10);
+            break;
+        default:
+            status();
+            break;
+    }
+
+    return 0;
+
+}
+
+int status(){
+    printf("KKKKKKKKKKKKKKKKKKKKKKKKKKk VAI SE FUDER KKKKKKK \n");
+    return 0;
+}
+
+int procfile(int argc,char *argv[]){
+    write(STDIN_FILENO,"Procesing\n",10);
+
+    //criar n-2 pipes para os filhos
+    int p[argc-2][2];
+    
+    for (int i = 2 ; i < argc ; i++){
+        //pipe 0 leitura
+        //pipe 1 escrita
+        //cria n-esimo pipe
+        if(i != argc-1){
+            if(pipe(p[i-2]) < 0){
+                perror("pipe");
+                _exit(1);
+            }
+        }
+        char transf [17 + strlen(argv[i])];
+        strcpy(transf,"./SDStore-transf/");
+        strcat(transf ,argv[i]);
+        //primeiro ciclo e 1 argumento
+        if(i == 2 && (i == (argc -1))){
+            
+            if(!fork()){
+                //close(pipe_fd)
+                int fd0 = open(argv[0], O_RDONLY,0666);
+                //ficehiro passa a ser stdin
+                dup2(fd0,0);
+                //close stdout e programa escreve no ficheiro pipe escrita n
+                int fd1 = open(argv[1],O_CREAT|O_WRONLY|O_TRUNC,0666);
+                //stdout passa a ser pipe de escrita n-2
+                dup2(fd1,STDOUT_FILENO);
+                //programa executa
+            
+                execl(transf ,transf,NULL);
+                perror("execl");
+                _exit(1);
+            
+            }
+        }
+        //primeiro ciclo
+        else if(i==2){
+
+            if(!fork()){
+                //close(pipe_fd)
+                //close do pipe que nao vamos usar
+                close(p[i-2][0]);
+                //close stdin e programa le o ficheiro fd0
+                int fd0 = open(argv[0], O_RDONLY,0666);
+                //ficehiro passa a ser stdin
+                dup2(fd0,STDIN_FILENO);
+                //close stdout e programa escreve no ficheiro pipe escrita n
+                //int fd1 = write(argv[1],O_CREAT|O_WRONLY|O_TRUNC,0666);
+                //stdout passa a ser pipe de escrita n-2
+                dup2(p[i-2][1],STDOUT_FILENO);
+                //programa executa
+            
+            
+            execl(transf ,transf,NULL);
+            perror("execl");
+            _exit(1);
+            
+            }
+        }
+        //ciclos seguintes
+        else if ((argc-1)!= i){
+
+            if(!fork()){
+                //close do pipe que nao vamos usar
+                close(p[i-2][0]);
+                //close stdin e programa le o pipe anterior
+                dup2(p[i-3][0],STDIN_FILENO);
+                //ficehiro passa a ser stdin
+            
+                //close stdout e programa escreve no ficheiro pipe escrita n
+                //int fd1 = write(argv[1],O_CREAT|O_WRONLY|O_TRUNC,0666);
+                //stdout passa a ser pipe de escrita n-2
+                dup2(p[i-2][1],STDOUT_FILENO);
+                //programa executa
+                
+                execl(transf ,transf,NULL);
+                perror("execl");
+                _exit(1);
+            
+            }
+        }
+        //ultimo ciclo
+        else{
+
+            if(!fork()){
+
+                int fd1 = open(argv[1],O_CREAT|O_WRONLY|O_TRUNC,0666);
+                dup2(fd1,STDOUT_FILENO);
+                dup2(p[i-3][0],STDIN_FILENO);
+                execl(transf ,transf,NULL);
+                
+                _exit(1);
+            }
+            
+        }
+        //fecha pipe de escrita
+        close(p[i-2][1]);
+        //fecha pipe de leitura anterior
+        if(i!=2)close(p[i-3][0]);
+        if(i == (argc-1))close(p[i-2][0]);
+        wait(NULL);
+        
+    }
+ 
+    return 0;
+
+
+}
